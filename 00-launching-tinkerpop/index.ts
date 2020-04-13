69e96362e3394cfeaae28c87109d5eb3fb4ca035
softwarewright <softwarebywright@gmail.com>
2020-04-12 22:48:42 -0600

    Starting neptune project

--- 00-launching-tinkerpop/index.ts
+++ 00-launching-tinkerpop/index.ts
@@ -3,25 +3,17 @@ import gremlin from 'gremlin';
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
 const Graph = gremlin.structure.Graph;
 
-console.log('Attempting to connect: ');
-
 const connection = new DriverRemoteConnection('wss://localhost:8182/gremlin', {});
-
-console.log('Connected: ', connection);
 
 const graph = new Graph();
 const g = graph.traversal().withRemote(connection);
 
-console.log('G: ', g);
 
 const setUpGraph = async () => {
-    console.log("Setting up the graph");
-    await g.addV('face').property('id', '1').property('name', 'darrius');
-    console.log("Created the Node");
-
-    const valueMap = await g.V().valueMap();
-
-    console.log("The value map: ", valueMap);
+    // await g.addV('person').property('id', 2).property('name', 'Keionne').next();
+    const values = await g.V();
+    console.log("Values:", values);
+    connection.close();
 }
 
 setUpGraph();
